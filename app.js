b85eaf78a345d615b34fd698fec7222d52c3cf3a
Ilya Kushlianski <kushliansky@gmail.com>
2017-10-11 14:22:23 +0300

    add post requests and form handling

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
+
 
 app.get('/', function(req, res){
   res.render('index');
 });
+app.get('/contact', function(req, res){
+  res.render('contact', {qs: req.query});
+});
+app.post('/contact', urlencodedParser, function(req, res){
+  console.log(req.body);
+  res.render('contact-success', {data: req.body});
+});
 app.get('/profile/:name', function(req, res){
   var data = {age: 26, job: 'ninja', hobbies: ['pes2017', 'fighting', 'fishing']};
-  res.render('profile', {person: req.params.name, data: data});
+  res.render('profile', {person: req.query, data: data});
 });
 
 app.listen(3000);
